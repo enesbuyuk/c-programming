6d013fa481179fa5cc6eb77be3a54c7928170a6b
Enes Büyük <contact@enesbuyuk.com>
2023-11-28 23:29:48 +0300

    Update eulers-number.c

--- homeworks/eulers-number.c
+++ homeworks/eulers-number.c
@@ -2,6 +2,7 @@
 
 int main() {
     double myInput, result=1;
+    printf("Please enter the value of n for calculating the number e: ");
     scanf("%lf",&myInput);
     for (int i =0; i<myInput;i++){
         result *= 1+1/myInput;
