823b1ff8562ecf77d03672afe88faafffccda343
Enes Büyük <contact@enesbuyuk.com>
2023-12-07 23:21:39 +0300

    Create move-to-nplus1.c

---
+++ move-to-nplus1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Please enter the length of the array: ");
+
+    if(scanf("%d",&n) == 0){
+        printf("You must enter a integer!");
+        return 0;
+    }
+
+    int oldArray[n],newArray[n];
+
+    for(int i=0;i<n;i++){
+        if(scanf("%d",&oldArray[i]) == 0){
+            printf("You must enter a integer!");
+            return 0;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        (i == n-1) ? (newArray[0] = oldArray[i]) : (newArray[i+1] = oldArray[i]);
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d ",newArray[i]);
+    }
+
+    return 0;
+}
